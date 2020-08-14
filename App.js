07ddd0de4d56fcf25e7da5d64e8d7af146c07a15
musa <seyhmus.aba@outlook.com.tr>
2020-08-15 00:47:01 +0300

    ilk projemm

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View,Text} from "react-native";
+
+class Musa extends React.Component {
+  render () {
+  
+  return <Text>Musa</Text>;
+  
+  
+}
+
+}
+
+export default Musa;
